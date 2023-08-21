7ed7800f1337a921071ff5e1c4c925a1f4dc1dad
Eduardo Ricardo <eduwric@gmail.com>
2023-08-21 13:30:41 -0300

    ci: :green_heart: fix database init

--- initdb.sql
+++ initdb.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS pessoas (
     id uuid PRIMARY KEY,
     apelido varchar(32) NOT NULL UNIQUE,
     nome varchar(100) NOT NULL,
-    nascimento date
+    nascimento date,
     stacks text
 );
